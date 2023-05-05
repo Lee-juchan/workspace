1621a9b5a6f095eb35b534edb70415e2edf6bc2e
leejuchan <leejuchan317@gmail.com>
2023-05-05 11:37:30 +0900

    [Style] edit comments, code styles

--- 23/javascript_webSystemDevelopment/assignment/arrayMethods.js
+++ 23/javascript_webSystemDevelopment/assignment/arrayMethods.js
@@ -7,9 +7,7 @@
 
 // < 순회 함수 >
 // 콜백함수를 인자로 받는다, Array, Map, Set을 순회하며 콜백함수에게 값을 전달한다.
-// 콜백 함수의 인자로 전달되는 값 -> callback(curValue, curIndex, Array) : 현재값, 현재 인덱스, 객체(Array, Map, Set 자체)
-
-let obj = { a: "test" };
+// 콜백함수의 인자로 전달되는 값 -> callback(curValue, curIndex, Array) : 현재값, 현재 인덱스, 객체(Array, Map, Set 자체)
 
 // 1. Array
 
@@ -20,7 +18,7 @@ Array.prototype.forEach = function (callback, thisArg) {
     }
 };
 // ex
-console.log("forEach ----------");
+console.log("forEach : ");
 let forEachEx = [1, 2, 3].forEach((value, index) => console.log(value * index));
 
 
@@ -35,14 +33,13 @@ Array.prototype.map = function (callback, thisArg) {
     return mappedArr;
 };
 // ex
-console.log("map ----------");
 let mapEx = [1, 2, 3].map((value, index) => value * index);
-console.log(mapEx);
+console.log("map : " + mapEx);
 
 
 
 // flatMap : map과 동일 + 한층 벗겨진 새 배열 생성      (return : 생성된 배열)
-Array.prototype.FlatMap = function (callback, thisArg) {
+Array.prototype.flatMap = function (callback, thisArg) {
     let mappedArr = [];
     let idx = 0;
 
@@ -60,11 +57,8 @@ Array.prototype.FlatMap = function (callback, thisArg) {
     return mappedArr;
 };
 // ex
-console.log("flatMap ----------");
-let flatMapEx = [1, 2, 3].FlatMap((value, idx) => [value, idx]);
-// let compare = [1, 2, 3].map((value, idx) => [value, idx]);
-// console.log(flatMapEx, compare);
-console.log(flatMapEx);
+let flatMapEx = [1, 2, 3].flatMap((value, idx) => [value, idx]);
+console.log("flatMap : " + flatMapEx);
 
 
 
@@ -82,9 +76,8 @@ Array.prototype.filter = function (callback, thisArg) {
     return filteredArr;
 };
 // ex
-console.log("filter ----------");
 let filterEx = [1, 2, 3].filter(value => value % 2 == 0);
-console.log(filterEx);
+console.log("filter : " + filterEx);
 
 
 
@@ -99,9 +92,8 @@ Array.prototype.some = function (callback, thisArg) {
     return false;
 };
 // ex
-console.log("some ----------");
 let someEx = [1, 2, 3].some(value => value >= 2);
-console.log(someEx);
+console.log("some : " + someEx);
 
 
 
@@ -116,9 +108,8 @@ Array.prototype.every = function (callback, thisArg) {
     return true;
 };
 // ex
-console.log("every ----------");
 let everyEx = [1, 2, 3].every(value => value >= 2);
-console.log(everyEx);
+console.log("every : " + everyEx);
 
 
 
@@ -133,14 +124,13 @@ Array.prototype.find = function (callback, thisArg) {
     return undefined;
 };
 // ex
-console.log("find ----------");
 let findEx = [1, 2, 3].find(value => value >= 2);
-console.log(findEx);
+console.log("find : " + findEx);
 
 
 
 // findIndex : 콜백함수의 리턴값이 true인 첫번째 요소의 인덱스 반환       (return : 배열 인덱스 | -1)
-Array.prototype.FindIndex = function (callback, thisArg) {
+Array.prototype.findIndex = function (callback, thisArg) {
     for (let i = 0; i < this.length; i++) {
         let condition = callback.call(thisArg, this[i], i, this);
         if (condition) {
@@ -150,13 +140,12 @@ Array.prototype.FindIndex = function (callback, thisArg) {
     return -1;
 };
 // ex
-console.log("findIndex ----------");
 let findIndexEx = [1, 2, 3].findIndex(value => value >= 2);
-console.log(findIndexEx);
+console.log("findIndex : " + findIndexEx);
 
 
 
-// from : 유사배열, 순회가능 객체로 배열 생성       (return : 생성된 배열)
+// from : 유사배열 객체, 순회가능 객체로 배열 생성     (return : 생성된 배열)
 Array.prototype.from = function (arrLike) {
     let arr = [];
     let idx = 0;
@@ -166,7 +155,6 @@ Array.prototype.from = function (arrLike) {
     return arr;
 };
 //ex
-console.log("from ----------");
 let o = {
     0: "a",
     1: "b",
@@ -177,25 +165,25 @@ let s = "string";
 
 let fromEx1 = Array.prototype.from(o);
 let fromEx2 = Array.prototype.from(s);
+console.log("from : ");
 console.log(fromEx1, fromEx2);
 
 
 
 // 2. Map, Set
 
-// map : 다양한 타입의 키 값      (<-> object : String type 키 값만 존재)
-// set : 키 값 x, 중복 허용 x
+// Map : 다양한 타입의 키 값      (<-> object : String type 키 값만 존재)
+// Set : 키 값 x, 중복 허용 x
 
-
-// map.forEach : 콜백함수 인자로 callback(curValue, curKey, Map) 전달
-Map.prototype.forEach = function(callback, thisArg) {
-    for(let element of this) {
-        callback.call(thisArg || global, element[1], element[0], this);
+// (Map) forEach : 콜백함수 인자로 callback(curValue, curKey, Map) 전달
+Map.prototype.forEach = function (callback, thisArg) {
+    for (let entries of this) {
+        callback.call(thisArg || global, entries[1], entries[0], this);
     }
 }
-
+// ex
+console.log("(Map) forEach : ");
 let myMap = new Map([
-    ["a", 3],
     ["3", 7],
     [3, 7]
 ]);
@@ -203,12 +191,13 @@ myMap.forEach((value, index) => console.log(value + index));
 
 
 
-// set.forEach : 콜백함수 인자로 callback(curValue, curValue, Set) 전달 (set은 키가 없음)
-Set.prototype.forEach = function(callback, thisArg) {
-    for(let value of this) {
+// (Set) forEach : 콜백함수 인자로 callback(curValue, curValue, Set) 전달 (set은 키가 없음)
+Set.prototype.forEach = function (callback, thisArg) {
+    for (let value of this) {
         callback.call(thisArg || global, value, value, this);
     }
 }
-
-let mySet = new Set([1,2,3]);
+// ex
+console.log("(Set) forEach : ");
+let mySet = new Set([1, 1, 2, 2, 3]);
 mySet.forEach((value) => console.log(value * 2));
